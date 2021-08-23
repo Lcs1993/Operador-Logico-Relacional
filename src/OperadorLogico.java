ee74bec9312d9cc5ab24ed5a2bb973c423333beb
Lcs1993 <ldacosta461@gmail.com>
2021-08-23 08:59:18 -0300

    continuação da aula 8 operador logico

---
+++ src/OperadorLogico.java
@@ -0,0 +1,17 @@
+
+public class OperadorLogico {
+
+	public static void main(String[] args) {
+		System.out.println("operador and &&");
+		System.out.println(true && false);
+		System.out.println(true && true);
+		System.out.println(false && false);
+		System.out.println(false && true);
+		System.out.println("operador or ||");
+		System.out.println(true || false);
+		System.out.println(true || true);
+		System.out.println(false || false);
+		System.out.println(false || true);
+	}
+
+}
