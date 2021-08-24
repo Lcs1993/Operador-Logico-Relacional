78d1cfc7b35c75dbe2093dc520e255065fc84309
Lcs1993 <ldacosta461@gmail.com>
2021-08-24 09:28:36 -0300

    adicionando nova condição

--- src/OperadorLogico.java
+++ src/OperadorLogico.java
@@ -2,7 +2,7 @@
 public class OperadorLogico {
 
 	public static void main(String[] args) {
-		System.out.println("operador and &&");
+		/**System.out.println("operador and &&");
 		System.out.println(true && false);
 		System.out.println(true && true);
 		System.out.println(false && false);
@@ -11,7 +11,15 @@ public class OperadorLogico {
 		System.out.println(true || false);
 		System.out.println(true || true);
 		System.out.println(false || false);
-		System.out.println(false || true);
+		System.out.println(false || true);*/
+		
+		int x,y,z;
+		x=4;
+		y=7;
+		z=12;
+		boolean r;
+		r=(x<y ^ y>z)?true:false;
+		System.out.println(r);
 	}
 
 }
